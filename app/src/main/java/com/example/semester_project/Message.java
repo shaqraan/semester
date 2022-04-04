9e07fcd3fb7947d5e5f6092bc9926c7e96fc222b
shaqraan <shaqraan113@gmail.com>
2022-04-04 09:09:32 +0500

    Semester project sample

---
+++ app/src/main/java/com/example/semester_project/Message.java
@@ -0,0 +1,9 @@
+package com.example.semester_project;
+import android.content.Context;
+import android.widget.Toast;
+
+public class Message {
+    public static void message(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/com/example/semester_project/activity_sqlite_operations.java
@@ -0,0 +1,14 @@
+package com.example.semester_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_sqlite_operations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sqlite_operations);
+    }
+}
